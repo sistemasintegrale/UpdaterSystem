227a1be50e756bb3c7c0977d039fcd2c1b1e27a2
sistemasintegrale <side4367@gmail.com>
2023-02-20 17:17:27 -0500

    Tercer Publish

--- SGE.UpdaterApp/SGE.UpdaterApp/Begin.Designer.cs
+++ SGE.UpdaterApp/SGE.UpdaterApp/Begin.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
             this.SuspendLayout();
             // 
             // guna2Elipse1
@@ -47,12 +49,32 @@
             this.metroLabel1.TabIndex = 0;
             this.metroLabel1.Text = "metroLabel1";
             // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(359, 216);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(83, 19);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "metroLabel2";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(512, 105);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(83, 19);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "metroLabel3";
+            // 
             // Begin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroLabel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Begin";
@@ -66,5 +88,7 @@
 
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
         private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
     }
 }
