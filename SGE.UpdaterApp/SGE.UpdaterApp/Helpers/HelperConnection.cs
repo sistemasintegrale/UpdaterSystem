19316ced8fd94683a66f16e06e8a4a899eb85c35
sistemasintegrale <side4367@gmail.com>
2023-11-02 09:57:00 -0500

    asdf

--- SGE.UpdaterApp/SGE.UpdaterApp/Helpers/HelperConnection.cs
+++ SGE.UpdaterApp/SGE.UpdaterApp/Helpers/HelperConnection.cs
@@ -30,7 +30,7 @@ namespace SGE.UpdaterApp.Helpers
                     strConexion = "Server=novaglass.database.windows.net,1433;Initial Catalog=SGE_NOVAFLAT;Persist Security Info=False;User ID=adminnova;Password = Novaazure$9; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30; ";
                     break;
                 case 6: // NOVA MOTOS
-                    strConexion = "Server=novamotorssql.database.windows.net,1433;Initial Catalog=SGE_NOVAMOTOS;Persist Security Info=False;User ID=adminsql;Password = novamotors$9; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30; "; 
+                    strConexion = "Data Source=tcp:95.217.194.247,1433;Initial Catalog=sistemasintegrales_com_NM;Persist Security Info=True;User ID=sistemasintegrales_com_de;Password=rogola2012;MultipleActiveResultSets = True; TrustServerCertificate = False; Encrypt = False"; 
                     break;
                 case 7: //CALZADOS JAGUAR
                     strConexion = "Data Source=tcp:95.217.194.247,1433;Initial Catalog=jaguar_com_pe_CJ;Persist Security Info=True;User ID=jaguar_com_pe_CJ;Password=eY-68j#0D;MultipleActiveResultSets = True; TrustServerCertificate = False; Encrypt = False";
