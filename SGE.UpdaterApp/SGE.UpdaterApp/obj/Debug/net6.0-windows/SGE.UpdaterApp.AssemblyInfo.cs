4935f9f67e034f502b6a4c81bed5efc593200f79
sistemasintegrale <side4367@gmail.com>
2024-07-12 17:54:05 -0500

    as

--- SGE.UpdaterApp/SGE.UpdaterApp/obj/Debug/net6.0-windows/SGE.UpdaterApp.AssemblyInfo.cs
+++ SGE.UpdaterApp/SGE.UpdaterApp/obj/Debug/net6.0-windows/SGE.UpdaterApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.UpdaterApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f6563de1ee0abf9cec48a036e2d59820424bd22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c34880e6352941e1d0bbdbc51f4493cf4ecc1b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.UpdaterApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.UpdaterApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
